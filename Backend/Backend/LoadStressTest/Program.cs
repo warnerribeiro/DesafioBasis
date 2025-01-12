cb5febeb6c542ef8d60971c2fb35cbe1fa4dca06
Warner Ribeiro Arantes <44380447+warnerribeiro@users.noreply.github.com>
2025-01-12 10:31:01 -0300

    Criação do projeto de teste de Carga e Estresse

---
+++ Backend/Backend/LoadStressTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using NBomber.CSharp;
+
+using var httpClient = new HttpClient();
+
+var scenario = Scenario.Create("hello_world_scenario", async context =>
+{
+    var response = await httpClient.GetAsync("https://nbomber.com");
+
+    return response.IsSuccessStatusCode
+        ? Response.Ok()
+        : Response.Fail();
+})
+.WithoutWarmUp()
+.WithLoadSimulations(
+    Simulation.Inject(rate: 10,
+                      interval: TimeSpan.FromSeconds(1),
+                      during: TimeSpan.FromSeconds(30))
+);
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .Run();
